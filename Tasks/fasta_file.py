443660964a10a82d10723d458a99584aa05a7874
phabel-LD <80180307+phabel-LD@users.noreply.github.com>
2021-04-30 20:16:32 -0500

    Update fasta_file.py

--- Tasks/fasta_file.py
+++ Tasks/fasta_file.py
@@ -1,3 +1,57 @@
+'''
+## NAME:
+  fasta_file.py
+  
+## LANGUAGE & VERSION:
+  python 3.9.2
+  
+## AUTHOR:
+  Phabel Antonio Lopez Delgado <phabel2001@gmail.com>
+  
+## DATE:
+  April 30th, 2021.
+  
+## DESCRIPTION & LOGIC:
+  This script takes a txt file with NT sequences and creates a fasta file with them.
+
+## USAGE:
+  This script is aimed for python3. The main method implemented is: open(), readlines()
+
+## ARGUMENTS:
+  No extra arguments needed.
+
+## INPUT - OUTPUT:
+  Input: 4_dna_sequences.txt
+  Output: dna_sequences.fasta
+    
+## EXAMPLES
+  4_dna_sequences.txt
+            seq_1 = "ATCGTACGATCGATCGATCGCTAGACGTATCG"
+            seq_2 = "actgatcgacgatcgatcgatcacgact"
+            seq_3 = "ACTGAC-ACTGT-ACTGTA----CATGTG"
+            
+   dna_sequences.fasta
+            >seq_1
+            ATCGTACGATCGATCGATCGCTAGACGTATCG
+            >seq_2
+            ACTGATCGACGATCGATCGATCACGACT
+            >seq_3
+            ACTGACACTGTACTGTACATGTG
+   
+## SOFTWARE REQUIREMENTS:
+  This script runs with python 3.9.2. No extra libraries were used.
+    
+## EXTRA COMMENTS:
+    The string modifications for the lines will depend on the input file's structure.
+    With other input files, other modifications may be needed o not.
+
+## LAST MODIFICATION:
+  Phabel Antonio Lopez Delgado: April 30th, 2021.
+  
+## GITHUB
+  https://github.com/phabel-LD/python_class/edit/master/Tasks/fasta_file.py
+  
+'''
 
 ## Abrir archivo como lectura con open("","r+")
 data = open("../Listas_y_loops/4_dna_sequences.txt", "r+") ## Recordar comprobar la ruta del archivo input.
